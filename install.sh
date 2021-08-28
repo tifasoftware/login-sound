8ba8cbd8814dcb3838272697cc9c501e9286ccbc
tifasoftware <83831269+tifasoftware@users.noreply.github.com>
2021-08-28 16:06:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing login-sound"
 echo "By Tifa Software"
-
+echo "-------------"
 echo "Creating Directories"
 mkdir -p /usr/share/sounds/login
 
@@ -11,8 +11,10 @@ cp startup.ogg /usr/share/sounds/login/
 
 echo "Ensuring file is executable"
 chmod +x /usr/local/bin/login-sound
-
+echo "-------------"
 echo "Installation Done!"
+echo "-------------"
 echo "Run login-sound to test it, and add the command to startup applications"
-echo "If you don't know how consult your DE documentations."
+echo "If you don't know how to do that, look in your DE's settings application."
+echo "-------------"
 echo "Have Fun! :)"
